96bb8c5050076c6125d58f6fbc3d27d484fa27fe
Lance Bailey <lancebailey@lances-air.lan>
2021-12-31 12:53:02 -0500

    added header

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,9 @@
 import React from "react";
-
+import UsersList from "../components/UsersList";
 const Users = () =>{
-    return (<h2>Users Works</h2>)
+    const USERS = [{id: 'u1', name:"Lance Bailey", image:'https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fentries%2Ficons%2Foriginal%2F000%2F037%2F171%2Fcover2.jpg', places: 3}];
+
+    return <UsersList items={USERS} />
 }
 
 export default Users;
